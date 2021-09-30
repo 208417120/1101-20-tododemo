05fbb378e28da383c1bc20d4390f37f0dfddf4fd
Your Name208417120 <208417120@gms.tku.edu.tw>
2021-09-30 21:50:17 +0800

    first commit

---
+++ src/App_20.js
@@ -0,0 +1,15 @@
+import Todo_20 from './components/Todo_20'
+import './App.css';
+
+function App_20() {
+  return (
+    <div>
+     <h1>My todos</h1>
+     <Todo_20 text='Learn React' />
+     <Todo_20 text='Learn sol' />
+     <Todo_20 text='Learn NextJS' />
+    </div>
+  );
+}
+
+export default App_20;
